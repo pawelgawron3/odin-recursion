1a50cef68d6ffdffac14aa8dbba14b5d6d666a0a
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-24 17:46:17 +0200

    Create fibsRec() function using the recursion

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -7,3 +7,15 @@ function fibs(n) {
 }
 
 console.log(fibs(8));
+
+function fibsRec(n) {
+  if (n === 1) {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  } else {
+    return fibsRec(n - 1) + fibsRec(n - 2);
+  }
+}
+
+console.log(fibsRec(8));
